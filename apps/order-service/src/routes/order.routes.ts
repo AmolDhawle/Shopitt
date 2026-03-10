83030ff192241630c56e1814ea650f2e59ee0fb1
AmolDhawle <amoldhawle2@gmail.com>
2026-03-10 13:52:53 +0530

    feat: Seller Dashboard - Orders & Payments Pages
    - Completed the **Orders Page** UI: Displays a list of seller's orders with filter and search functionality.
    - Developed the **Order Details Page**: Provides detailed information about each order, including order status, items, payment, and shipping information.
    - Created the **Payments Page** UI: Displays seller earnings, admin fee, and order payment status.
    - Refactored discount codes to be tied to **shops** instead of sellers.
    - Introduced **OrderGroups** in the order schema, allowing for grouping of related orders.
    - Added the  field in the  schema to associate orders with a group.
    - Created the **OrderTimelineStatus** enum to manage the order status timeline.
    - Added  field in the  schema to track status updates for each order.
    - Updated the **OrderStatusHistory** schema to capture individual status changes with optional notes.

--- apps/order-service/src/routes/order.routes.ts
+++ apps/order-service/src/routes/order.routes.ts
@@ -2,14 +2,30 @@ import express, { Router } from 'express';
 import {
   createPaymentIntent,
   createPaymentSession,
+  getOrderDetails,
+  getSellerOrders,
+  updateOrderStatus,
   verifyingPaymentSession,
 } from '../controllers/order.controller';
-import { requireAuth } from '@shopitt/middleware';
+import { authorizeRole, requireAuth } from '@shopitt/middleware';
 
 const router: Router = express.Router();
 
 router.post('/create-payment-intent', requireAuth, createPaymentIntent);
 router.post('/create-payment-session', requireAuth, createPaymentSession);
 router.get('/verifying-payment-session', requireAuth, verifyingPaymentSession);
+router.get(
+  '/get-seller-orders',
+  requireAuth,
+  authorizeRole('seller'),
+  getSellerOrders,
+);
+router.get('/get-order-details/:id', requireAuth, getOrderDetails);
+router.put(
+  '/update-status/:id',
+  requireAuth,
+  authorizeRole('seller'),
+  updateOrderStatus,
+);
 
 export default router;
